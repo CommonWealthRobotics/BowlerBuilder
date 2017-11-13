e7742bb65acdecf762f10957154593c8d24bdf5a
Ryan Benasutti <firey45@gmail.com>
2017-11-13 17:59:32 -0500

    Added basic rendering of CSG

--- src/main/java/com/neuronrobotics/bowlerbuilder/controller/CADModelViewerController.java
+++ src/main/java/com/neuronrobotics/bowlerbuilder/controller/CADModelViewerController.java
@@ -1,43 +1,79 @@
 package com.neuronrobotics.bowlerbuilder.controller;
 
+import eu.mihosoft.jcsg.CSG;
+import eu.mihosoft.jcsg.Cube;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.DepthTest;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.SubScene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Box;
+import javafx.scene.shape.CullFace;
 import javafx.scene.shape.DrawMode;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
+import javafx.util.Duration;
 
 public class CADModelViewerController implements Initializable {
 
   @FXML
   private AnchorPane root;
 
+  //Viewing camera and its transforms
+  private final PerspectiveCamera camera;
+  private final Rotate rotateX;
+  private final Rotate rotateY;
+  private final Rotate rotateZ;
+  private final Translate translate;
+
+  public CADModelViewerController() {
+    rotateX = new Rotate(0, Rotate.X_AXIS);
+    rotateY = new Rotate(0, Rotate.Y_AXIS);
+    rotateZ = new Rotate(0, Rotate.Z_AXIS);
+    translate = new Translate(0, 0, -15);
+    camera = new PerspectiveCamera(true);
+    camera.getTransforms().addAll(
+        rotateX,
+        rotateY,
+        rotateZ,
+        translate);
+  }
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    // Box
-    Box testBox = new Box(5, 5, 5);
-    testBox.setMaterial(new PhongMaterial(Color.RED));
-    testBox.setDrawMode(DrawMode.LINE);
+    CSG cube = new Cube(1, 1, 1).toCSG();
 
-    // Create and position camera
-    PerspectiveCamera camera = new PerspectiveCamera(true);
-    camera.getTransforms().addAll(
-        new Rotate(-20, Rotate.Y_AXIS),
-        new Rotate(-20, Rotate.X_AXIS),
-        new Translate(0, 0, -15));
+    Consumer<Rotate> makeTimelines = rotate -> {
+      Timeline timeline = new Timeline(
+          new KeyFrame(Duration.seconds(0), new KeyValue(rotate.angleProperty(), 0)),
+          new KeyFrame(Duration.seconds(15), new KeyValue(rotate.angleProperty(), 360)));
+      timeline.setCycleCount(Animation.INDEFINITE);
+      timeline.play();
+    };
+    makeTimelines.accept(rotateX);
+    makeTimelines.accept(rotateY);
+    makeTimelines.accept(rotateZ);
 
     // Build the Scene Graph
     Group sceneGraph = new Group();
     sceneGraph.getChildren().add(camera);
-    sceneGraph.getChildren().add(testBox);
+    cube.toJavaFXMesh().getAsMeshViews().forEach(mesh -> {
+      mesh.setMaterial(new PhongMaterial(Color.RED));
+      mesh.setDrawMode(DrawMode.FILL);
+      mesh.setDepthTest(DepthTest.ENABLE);
+      mesh.setCullFace(CullFace.BACK);
+      sceneGraph.getChildren().add(mesh);
+    });
 
     // Use a SubScene
     SubScene subScene = new SubScene(sceneGraph, 300, 300);
