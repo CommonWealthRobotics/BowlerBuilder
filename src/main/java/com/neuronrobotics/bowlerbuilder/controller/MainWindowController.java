5928cab41ca0a7dab8675411a372d484eebd2e78
Ryan Benasutti <firey45@gmail.com>
2017-12-17 22:38:11 -0500

    PMD

--- src/main/java/com/neuronrobotics/bowlerbuilder/controller/MainWindowController.java
+++ src/main/java/com/neuronrobotics/bowlerbuilder/controller/MainWindowController.java
@@ -21,7 +21,6 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
--- src/main/java/com/neuronrobotics/bowlerbuilder/controller/view/PreferencesController.java
+++ src/main/java/com/neuronrobotics/bowlerbuilder/controller/view/PreferencesController.java
@@ -1,7 +1,6 @@
 package com.neuronrobotics.bowlerbuilder.controller.view;
 
 import java.net.URL;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 
