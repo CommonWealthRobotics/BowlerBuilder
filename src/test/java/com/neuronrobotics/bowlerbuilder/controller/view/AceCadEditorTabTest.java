9e169864f0b24791c2704bb010fa1d812cadefff
Ryan Benasutti <firey45@gmail.com>
2017-12-23 14:47:43 -0500

    Added AceCadEditorTab test

---
+++ src/test/java/com/neuronrobotics/bowlerbuilder/controller/view/AceCadEditorTabTest.java
@@ -0,0 +1,36 @@
+package com.neuronrobotics.bowlerbuilder.controller.view;
+
+import static org.junit.Assert.assertTrue;
+
+import com.neuronrobotics.bowlerbuilder.AutoClosingApplicationTest;
+import com.neuronrobotics.bowlerbuilder.FxUtil;
+import eu.mihosoft.vrl.v3d.CSG;
+import java.util.ArrayList;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+
+public class AceCadEditorTabTest extends AutoClosingApplicationTest {
+
+  private AceCadEditorTab tab;
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    tab = new AceCadEditorTab("");
+    stage.setScene(tab.getView().getScene());
+    stage.show();
+  }
+
+  @Test
+  void basicTest() throws Exception {
+    FxUtil.runFX(() ->
+        tab.getController().getScriptEditor().insertAtCursor("return new Cube(1,1,1).toCSG();"));
+    Object result = FxUtil.returnFX(() -> tab.getController().getScriptRunner().runScript(
+        tab.getController().getScriptEditor().getText(),
+        new ArrayList<>(),
+        "AwareGroovy"
+    ));
+
+    assertTrue(result instanceof CSG);
+  }
+
+}
