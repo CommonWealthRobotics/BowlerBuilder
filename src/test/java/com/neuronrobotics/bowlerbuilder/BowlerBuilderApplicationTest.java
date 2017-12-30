51dcd66360bb962645e31430a4ae2ed48e71446f
Ryan Benasutti <firey45@gmail.com>
2017-12-30 00:03:53 -0500

    Added PreferenceService tests

--- src/test/java/com/neuronrobotics/bowlerbuilder/BowlerBuilderApplicationTest.java
+++ src/test/java/com/neuronrobotics/bowlerbuilder/BowlerBuilderApplicationTest.java
@@ -10,6 +10,7 @@ import org.testfx.api.FxToolkit;
 import org.testfx.util.WaitForAsyncUtils;
 
 class BowlerBuilderApplicationTest extends FxRobot {
+
   @BeforeEach
   void before() throws Exception {
     FxToolkit.registerPrimaryStage();
@@ -28,4 +29,5 @@ class BowlerBuilderApplicationTest extends FxRobot {
   void testStart() {
     //Nothing here, just test to make sure we start without exceptions
   }
+
 }
