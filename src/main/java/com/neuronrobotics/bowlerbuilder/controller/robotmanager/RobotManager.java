3ca28e9f577c3c9ce73186d710f2c89a3d5369db
Ryan Benasutti <firey45@gmail.com>
2018-02-09 19:22:03 -0500

    Validation is weirdly broken

--- src/main/java/com/neuronrobotics/bowlerbuilder/controller/robotmanager/RobotManager.java
+++ src/main/java/com/neuronrobotics/bowlerbuilder/controller/robotmanager/RobotManager.java
@@ -6,9 +6,7 @@ import com.neuronrobotics.bowlerbuilder.controller.CreatureLabController;
 import com.neuronrobotics.bowlerbuilder.controller.cadengine.CadEngine;
 import com.neuronrobotics.bowlerstudio.creature.MobileBaseCadManager;
 import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine;
-import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics;
 import com.neuronrobotics.sdk.addons.kinematics.MobileBase;
-import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
 import com.neuronrobotics.sdk.common.DeviceManager;
 import com.neuronrobotics.sdk.util.ThreadUtil;
 import java.io.IOException;
@@ -47,18 +45,18 @@ public class RobotManager {
       while (MobileBaseCadManager.get(mobileBase).getProcesIndictor().get() < 1) {
         ThreadUtil.wait(1000);
       }
-      while (true) {
-        DHParameterKinematics leg0 = mobileBase.getAllDHChains().get(0);
-        double zLift = 25;
-        TransformNR current = leg0.getCurrentPoseTarget();
-        current.translateZ(zLift);
-        leg0.setDesiredTaskSpaceTransform(current, 2.0);
-        ThreadUtil.wait(2000);
-
-        current.translateZ(-zLift);
-        leg0.setDesiredTaskSpaceTransform(current, 2.0);
-        ThreadUtil.wait(2000);
-      }
+//      while (true) {
+//        DHParameterKinematics leg0 = mobileBase.getAllDHChains().get(0);
+//        double zLift = 25;
+//        TransformNR current = leg0.getCurrentPoseTarget();
+//        current.translateZ(zLift);
+//        leg0.setDesiredTaskSpaceTransform(current, 2.0);
+//        ThreadUtil.wait(2000);
+//
+//        current.translateZ(-zLift);
+//        leg0.setDesiredTaskSpaceTransform(current, 2.0);
+//        ThreadUtil.wait(2000);
+//      }
     } catch (IOException e) {
       logger.log(Level.SEVERE,
           "Could not load assets for robot.\n" + Throwables.getStackTraceAsString(e));
--- src/main/java/com/neuronrobotics/bowlerbuilder/view/robotmanager/JogWidget.java
+++ src/main/java/com/neuronrobotics/bowlerbuilder/view/robotmanager/JogWidget.java
@@ -1,7 +1,10 @@
 package com.neuronrobotics.bowlerbuilder.view.robotmanager;
 
+import com.google.common.primitives.Floats;
 import com.neuronrobotics.bowlerstudio.assets.AssetFactory;
 import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics;
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
+import java.util.Optional;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -11,13 +14,27 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 public class JogWidget {
 
+  private final VBox view;
+  private final GridPane controlPane;
+  private final Button plusX;
+  private final Button minusX;
+  private final Button plusY;
+  private final Button minusY;
+  private final Button plusZ;
+  private final Button minusZ;
+  private final Button home;
+  private final Button addJoystick;
+  private final Button configureJoystick;
+  private final TextField speed;
+  private final TextField time;
+
   private DHParameterKinematics limb;
-  private VBox view;
-  private GridPane controlPane;
 
   public JogWidget(DHParameterKinematics limb) {
     this.limb = limb;
@@ -29,34 +46,66 @@ public class JogWidget {
     controlPane.setHgap(5);
     controlPane.setVgap(5);
 
-    Button plusX = addButton(AssetFactory.loadIcon("Plus-X.png"), "", 1, 0);
-    Button minusX = addButton(AssetFactory.loadIcon("Minus-X.png"), "", 1, 2);
+    plusX = addButton(AssetFactory.loadIcon("Plus-X.png"), "", 1, 0);
+    minusX = addButton(AssetFactory.loadIcon("Minus-X.png"), "", 1, 2);
 
-    Button plusY = addButton(AssetFactory.loadIcon("Plus-Y.png"), "", 0, 1);
-    Button minusY = addButton(AssetFactory.loadIcon("Minus-Y.png"), "", 2, 1);
+    plusY = addButton(AssetFactory.loadIcon("Plus-Y.png"), "", 0, 1);
+    minusY = addButton(AssetFactory.loadIcon("Minus-Y.png"), "", 2, 1);
 
-    Button plusZ = addButton(AssetFactory.loadIcon("Plus-Z.png"), "", 0, 0);
-    Button minusZ = addButton(AssetFactory.loadIcon("Minus-Z.png"), "", 2, 0);
+    plusZ = addButton(AssetFactory.loadIcon("Plus-Z.png"), "", 0, 0);
+    plusZ.setOnAction(event -> {
+      TransformNR current = limb.getCurrentPoseTarget();
+      current.translateZ(25);
+      try {
+        limb.setDesiredTaskSpaceTransform(current, 2);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    });
+    minusZ = addButton(AssetFactory.loadIcon("Minus-Z.png"), "", 2, 0);
 
-    Button home = addButton(AssetFactory.loadIcon("Home.png"), "", 1, 1);
+    home = addButton(AssetFactory.loadIcon("Home.png"), "", 1, 1);
 
-    Button addJoystick = addButton(AssetFactory.loadIcon("Add-Game-Controller.png"), "", 3, 0);
-    Button configureJoystick =
-        addButton(AssetFactory.loadIcon("Configure-Game-Controller.png"), "", 3, 1);
+    addJoystick = addButton(AssetFactory.loadIcon("Add-Game-Controller.png"), "", 3, 0);
+    configureJoystick = addButton(AssetFactory.loadIcon("Configure-Game-Controller.png"),
+        "", 3, 1);
 
     view.getChildren().add(controlPane);
 
-    TextField speed = new TextField("0.4");
-    speed.setPrefWidth(40);
+    speed = new TextField("0.4");
+    speed.setMinWidth(Region.USE_COMPUTED_SIZE);
+
+    /*ValidationSupport validator = new ValidationSupport();
+    validator.setValidationDecorator(
+        new StyleClassValidationDecoration("text-field-error",
+            "text-field-warning"));
+    validator.registerValidator(speed, false, (control, value) -> {
+      if (value instanceof String) {
+        return ValidationResult.fromMessageIf(control,
+            "Invalid number",
+            Severity.ERROR,
+            !validateAsFloatingPoint((String) value).isPresent());
+      }
+
+      return ValidationResult.fromMessageIf(control,
+          "Invalid number",
+          Severity.ERROR,
+          false);
+    });*/ //TODO: This shrinks the application on bad input for some reason
+
     HBox speedBox = new HBox(2, speed, new Label("m/s"));
     speedBox.setAlignment(Pos.CENTER_LEFT);
+    HBox.setHgrow(speedBox, Priority.NEVER);
 
-    TextField time = new TextField("0.03");
-    time.setPrefWidth(40);
+    time = new TextField("0.03");
+    time.setMinWidth(Region.USE_COMPUTED_SIZE);
     HBox timeBox = new HBox(2, time, new Label("sec"));
     timeBox.setAlignment(Pos.CENTER_LEFT);
+    HBox.setHgrow(timeBox, Priority.NEVER);
 
-    view.getChildren().add(new HBox(5, speedBox, timeBox));
+    HBox controlsBox = new HBox(5, speedBox, timeBox);
+    HBox.setHgrow(controlsBox, Priority.NEVER);
+    view.getChildren().add(controlsBox);
   }
 
   /**
@@ -73,6 +122,10 @@ public class JogWidget {
     out.setGraphic(icon);
     controlPane.add(out, x, y);
     return out;
+  }
+
+  private Optional<Float> validateAsFloatingPoint(String rawNumber) {
+    return Optional.ofNullable(Floats.tryParse(rawNumber));
   }
 
   public Node getView() {
