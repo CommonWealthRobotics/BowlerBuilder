eab1001db0dd4bdf75e4c01b20b408f83f6257c7
Ryan Benasutti <firey45@gmail.com>
2018-02-23 21:24:13 -0500

    Make AddLinkDialog look cleaner

--- src/main/java/com/neuronrobotics/bowlerbuilder/view/dialog/AddLinkDialog.java
+++ src/main/java/com/neuronrobotics/bowlerbuilder/view/dialog/AddLinkDialog.java
@@ -3,14 +3,13 @@ package com.neuronrobotics.bowlerbuilder.view.dialog;
 import com.google.common.primitives.Ints;
 import com.neuronrobotics.bowlerbuilder.view.dialog.util.ValidatedTextField;
 import java.util.Set;
-import javafx.geometry.Pos;
+import javafx.geometry.HPos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 
 public class AddLinkDialog extends Dialog<String[]> {
 
@@ -20,21 +19,28 @@ public class AddLinkDialog extends Dialog<String[]> {
     setTitle("Add Link");
 
     Label linkNameLabel = new Label("Link name");
+    GridPane.setHalignment(linkNameLabel, HPos.RIGHT);
     TextField linkNameField = new TextField();
-    HBox nameHBox = new HBox(5, linkNameLabel, linkNameField);
-    nameHBox.setAlignment(Pos.CENTER_LEFT);
+    GridPane.setHalignment(linkNameField, HPos.LEFT);
 
     Label hwIndexLabel = new Label("Hardware index");
+    GridPane.setHalignment(hwIndexLabel, HPos.RIGHT);
     ValidatedTextField hwIndexField = new ValidatedTextField("Invalid number",
         text -> {
           Integer result = Ints.tryParse(text);
           return result != null && !takenChannels.contains(result);
         });
-    HBox indexHBox = new HBox(5, hwIndexLabel, hwIndexField);
-    indexHBox.setAlignment(Pos.CENTER_LEFT);
+    GridPane.setHalignment(hwIndexField, HPos.LEFT);
 
-    VBox content = new VBox(5, nameHBox, indexHBox);
-    content.setAlignment(Pos.CENTER_LEFT);
+    GridPane content = new GridPane();
+    content.setHgap(5);
+    content.setVgap(5);
+
+    content.add(linkNameLabel, 0, 0);
+    content.add(linkNameField, 1, 0);
+    content.add(hwIndexLabel, 0, 1);
+    content.add(hwIndexField, 1, 1);
+
     getDialogPane().setContent(content);
     getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
 
