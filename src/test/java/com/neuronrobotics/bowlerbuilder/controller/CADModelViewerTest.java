019325c548f3f0bab371b039647869787f0849b7
Ryan Benasutti <firey45@gmail.com>
2018-02-27 14:33:15 -0500

    Make CADModelViewerTest into an ApplicationTest

--- src/test/java/com/neuronrobotics/bowlerbuilder/controller/CADModelViewerTest.java
+++ src/test/java/com/neuronrobotics/bowlerbuilder/controller/CADModelViewerTest.java
@@ -2,22 +2,40 @@ package com.neuronrobotics.bowlerbuilder.controller;
 
 import static org.junit.Assert.assertTrue;
 
+import com.neuronrobotics.bowlerbuilder.AutoClosingApplicationTest;
 import com.neuronrobotics.bowlerbuilder.BowlerBuilder;
+import com.neuronrobotics.bowlerbuilder.FxHelper;
 import com.neuronrobotics.bowlerbuilder.controller.module.CADModelViewerControllerModule;
 import eu.mihosoft.vrl.v3d.CSG;
 import eu.mihosoft.vrl.v3d.Cube;
 import java.util.Arrays;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-public class CADModelViewerTest {
-  private final CADModelViewerController controller =
-      BowlerBuilder.getInjector().createChildInjector(new CADModelViewerControllerModule())
-          .getInstance(CADModelViewerController.class);
+public class CADModelViewerTest extends AutoClosingApplicationTest {
+  private CADModelViewerController controller;
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(
+        getClass().getResource("../view/CADModelViewer.fxml"),
+        null,
+        null,
+        BowlerBuilder.getInjector()
+            .createChildInjector(new CADModelViewerControllerModule())::getInstance);
+    BorderPane mainWindow = loader.load();
+    controller = loader.getController();
+    stage.setScene(new Scene(mainWindow));
+    stage.show();
+  }
 
   @AfterEach
   void afterEach() {
-    controller.clearMeshes();
+    FxHelper.runAndWait(() -> controller.clearMeshes());
   }
 
   @Test
