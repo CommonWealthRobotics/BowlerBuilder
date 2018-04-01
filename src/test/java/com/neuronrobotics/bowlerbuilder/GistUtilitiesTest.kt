9878446f74b1a0da4bda16482c24df9ce5b851e3
Ryan Benasutti <firey45@gmail.com>
2018-03-31 21:04:06 -0400

    Issue #10: Add whitespace validation to GistUtilities.isValidCodeFileName
    Whitespace should not be allowed. Closes #10.

--- src/test/java/com/neuronrobotics/bowlerbuilder/GistUtilitiesTest.kt
+++ src/test/java/com/neuronrobotics/bowlerbuilder/GistUtilitiesTest.kt
@@ -8,23 +8,38 @@ import org.junit.jupiter.api.Test
 internal class GistUtilitiesTest {
 
     @Test
-    fun testIsValidCodeFileName1() {
+    fun `valid filename`() {
         assertTrue(GistUtilities.isValidCodeFileName("a.b").isPresent)
     }
 
     @Test
-    fun testIsValidCodeFileName2() {
+    fun `filename can contain underscores`() {
+        assertTrue(GistUtilities.isValidCodeFileName("a_c.b").isPresent)
+    }
+
+    @Test
+    fun `filename can contain hyphens`() {
+        assertTrue(GistUtilities.isValidCodeFileName("a-c.b").isPresent)
+    }
+
+    @Test
+    fun `filename needs an extension`() {
         assertFalse(GistUtilities.isValidCodeFileName("a").isPresent)
     }
 
     @Test
-    fun testIsValidCodeFileName3() {
+    fun `filename cannot have an empty extension`() {
         assertFalse(GistUtilities.isValidCodeFileName("a.").isPresent)
     }
 
     @Test
-    fun testIsValidCodeFileName4() {
+    fun `filename cannot be empty`() {
         assertFalse(GistUtilities.isValidCodeFileName("").isPresent)
+    }
+
+    @Test
+    fun `filename cannot contain spaces`() {
+        assertFalse(GistUtilities.isValidCodeFileName("a b.x").isPresent)
     }
 
 }
